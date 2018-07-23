a85359fd27a064b4fb321e45d5868db95c023239
artyom <cppg@cppg.co.uk>
2018-07-23 19:30:02 +0100

    added data augmentations: mixup and random erasing

--- data_v1_1.py
+++ data_v1_1.py
@@ -166,3 +166,93 @@ def load_dataset(files: List[str]) -> List[List[NpArray]]:
     pool = multiprocessing.Pool(processes=12)
     data = [x for x in tqdm(pool.imap(read_file, files), total=len(files))]
     return data
+
+
+def get_random_eraser(p: float = 0.5, s_l: float = 0.02, s_h: float = 0.4,
+                      r_1: float = 0.3, r_2: float = 1/0.3, v_l: float = 0,
+                      v_h: float = 1.0) -> Any:
+    """ An augmentation that erases part of the image. """
+    def eraser(input_img: NpArray) -> NpArray:
+        img_h, img_w, _ = input_img.shape
+        p_1 = np.random.rand()
+
+        if p_1 > p:
+            return input_img
+
+        while True:
+            s = np.random.uniform(s_l, s_h) * img_h * img_w
+            r = np.random.uniform(r_1, r_2)
+            w = int(np.sqrt(s / r))
+            h = int(np.sqrt(s * r))
+            left = np.random.randint(0, img_w)
+            top = np.random.randint(0, img_h)
+
+            if left + w <= img_w and top + h <= img_h:
+                break
+
+        c = np.random.uniform(v_l, v_h)
+        input_img[top:top + h, left:left + w, :] = c
+
+        return input_img
+
+    return eraser
+
+class MixupGenerator():
+    """ Implements mixup of audio clips. """
+    def __init__(self, X_train: NpArray, y_train: NpArray, batch_size: int = 32,
+                 alpha: float = 0.2, shuffle: bool = True, datagen: Any = None) -> None:
+        self.X_train = X_train
+        self.y_train = y_train
+        self.batch_size = batch_size
+        self.alpha = alpha
+        self.shuffle = shuffle
+        self.sample_num = len(X_train)
+        self.datagen = datagen
+
+    def __call__(self): # type: ignore
+        while True:
+            indexes = self.__get_exploration_order()
+            itr_num = int(len(indexes) // (self.batch_size * 2))
+
+            for i in range(itr_num):
+                batch_ids = indexes[i * self.batch_size * 2:(i + 1) * self.batch_size * 2]
+                X, y = self.__data_generation(batch_ids)
+
+                yield X, y
+
+    def __get_exploration_order(self) -> NpArray:
+        indexes = np.arange(self.sample_num)
+
+        if self.shuffle:
+            np.random.shuffle(indexes)
+
+        return indexes
+
+    def __data_generation(self, batch_ids: NpArray) -> Tuple[NpArray, NpArray]:
+        _, h, w, c = self.X_train.shape
+        l = np.random.beta(self.alpha, self.alpha, self.batch_size)
+        X_l = l.reshape(self.batch_size, 1, 1, 1)
+        y_l = l.reshape(self.batch_size, 1)
+
+        X1 = self.X_train[batch_ids[:self.batch_size]]
+        X2 = self.X_train[batch_ids[self.batch_size:]]
+        X = X1 * X_l + X2 * (1 - X_l)
+
+        if self.datagen:
+            for i in range(self.batch_size):
+                X[i] = self.datagen.random_transform(X[i])
+                X[i] = self.datagen.standardize(X[i])
+
+        if isinstance(self.y_train, list):
+            y = []
+
+            for y_train_ in self.y_train:
+                y1 = y_train_[batch_ids[:self.batch_size]]
+                y2 = y_train_[batch_ids[self.batch_size:]]
+                y.append(y1 * y_l + y2 * (1 - y_l))
+        else:
+            y1 = self.y_train[batch_ids[:self.batch_size]]
+            y2 = self.y_train[batch_ids[self.batch_size:]]
+            y = y1 * y_l + y2 * (1 - y_l)
+
+        return X, y
