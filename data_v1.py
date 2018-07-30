1e7626e3145f09235b1ef3b41148f855d226c169
artyom <cppg@cppg.co.uk>
2018-07-30 19:21:26 +0100

    added v80: LSTM with 20 MFCC coeffs (MAP@3 0.63)

--- data_v1.py
+++ data_v1.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3.6
 """ Original implementation of data loader. Returns 20 MFCC components. """
 
-import glob, multiprocessing, os, sys
+import glob, multiprocessing, os, pickle, sys
 from typing import *
 
-import numpy as np
+import numpy as np, pandas as pd
 import librosa
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelBinarizer
 from tqdm import tqdm
 
 NpArray = Any
 
 DATA_VERSION = 1
 
-SAMPLE_RATE = 44100
+TOPK                = 3
+SAMPLE_RATE         = 44100
 
 # Data parameters
 NUM_MFCC_ROWS       = 20
@@ -166,3 +168,112 @@ def load_dataset(files: List[str]) -> List[List[NpArray]]:
     pool = multiprocessing.Pool(processes=12)
     data = [x for x in tqdm(pool.imap(read_file, files), total=len(files))]
     return data
+
+def find_files(path: str) -> List[str]:
+    """ Reads whole dataset into np.array. """
+    files = sorted(list(glob.glob(os.path.join(path, "*.wav"))))
+
+    print("files: %d files found" % len(files))
+    return files
+
+def build_train_dataset(x: List[List[NpArray]], y: NpArray, indices:
+                        List[int]) -> \
+                        Tuple[NpArray, NpArray]:
+    """ Builds dataset with multiple items per sample. Returns (x, y). """
+    res_x, res_y = [], []
+
+    for idx in indices:
+        for sample in x[idx]:
+            res_x.append(sample)
+            res_y.append(y[idx])
+
+    return np.array(res_x), np.array(res_y)
+
+def build_test_dataset(x: List[List[NpArray]]) -> Tuple[NpArray, List[int]]:
+    """ Builds test dataset with multiple clips per sample, which will
+    be joined later somehow (maximum, geom. mean, etc).
+    Returns: (x_test, clips_per_sample). """
+    x_test, clips_per_sample = [], []
+
+    for sample in x:
+        clips_per_sample.append(len(sample))
+
+        for clip in sample:
+            x_test.append(clip)
+
+    return np.array(x_test), clips_per_sample
+
+def load_data(train_idx: NpArray, val_idx: NpArray) -> \
+              Tuple[NpArray, NpArray, NpArray, NpArray, NpArray, Any, List[int]]:
+    """ Loads all data. """
+    train_df = pd.read_csv("../data/train.csv", index_col="fname")
+
+    train_cache = "../output/train_cache_v%02d.pkl" % DATA_VERSION
+    test_cache = "../output/test_cache_v%02d.pkl" % DATA_VERSION
+
+    print("reading train dataset")
+    if os.path.exists(train_cache):
+        x = pickle.load(open(train_cache, "rb"))
+    else:
+        train_files = find_files("../data/audio_train/")
+        print("len(train_files)", len(train_files))
+
+        x = load_dataset(train_files)
+        pickle.dump(x, open(train_cache, "wb"))
+
+    # get whatever data metrics we need
+    label_binarizer = LabelBinarizer()
+    label_binarizer.fit(train_df["label"])
+
+    x_train, y_train = build_train_dataset(x, train_df["label"], train_idx)
+    x_val, y_val = build_train_dataset(x, train_df["label"], val_idx)
+
+    print("reading test dataset")
+    if os.path.exists(test_cache):
+        x_test = pickle.load(open(test_cache, "rb"))
+    else:
+        test_files = find_files("../data/audio_test/")
+        print("len(test_files)", len(test_files))
+
+        x_test = load_dataset(test_files)
+        pickle.dump(x_test, open(test_cache, "wb"))
+
+    x_test, clips_per_sample = build_test_dataset(x_test)
+
+    x_joined = np.concatenate([x_train, x_val])
+    mean, std = np.mean(x_joined), np.std(x_joined)
+    x_train = (x_train - mean) / std
+    x_val = (x_val - mean) / std
+    x_test = (x_test - mean) / std
+
+    y_train = label_binarizer.transform(y_train)
+    y_val = label_binarizer.transform(y_val)
+
+    print("x_train.shape", x_train.shape, "y_train.shape", y_train.shape)
+    print("x_val.shape", x_val.shape, "y_val.shape", y_val.shape)
+
+    return x_train, y_train, x_val, y_val, x_test, label_binarizer, clips_per_sample
+
+def map3_metric(predict: NpArray, ground_truth: NpArray) -> float:
+    """ Implements Mean Average Precision @ Top 3. """
+    results = []
+    assert(predict.shape[0] == ground_truth.shape[0])
+
+    for actual, pred in zip(ground_truth, predict):
+        actual = np.argmax(actual)
+
+        pred = np.argsort(pred)[-TOPK:]
+        pred = np.flip(pred, axis=0)
+
+        score = 0.0
+        num_hits = 0
+
+        for i, p in enumerate(pred):
+            if p == actual and p not in pred[:i]:
+                num_hits += 1
+                score += num_hits / (i + 1)
+
+        results.append(score)
+
+    assert(len(results) == predict.shape[0])
+    return np.mean(results)
