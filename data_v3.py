3a1690925f2125f61f5443794a41b533340ec9c4
artyom <cppg@cppg.co.uk>
2018-07-30 11:56:21 +0100

    added implementations of Conv1D

---
+++ data_v3.py
@@ -0,0 +1,313 @@
+#!/usr/bin/python3.6
+""" Data loader which returns raw sound data. """
+
+import glob, multiprocessing, os, pickle, sys
+from typing import *
+
+import numpy as np
+import pandas as pd
+import librosa
+import keras
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+from sklearn.preprocessing import LabelBinarizer
+
+
+DATA_VERSION    = 0x3
+NpArray         = Any
+
+TOPK            = 3
+NUM_CLASSES     = 41
+SAMPLE_RATE     = 44100
+
+
+# Data parameters
+SILENCE_THRESHOLD   = 1e-1
+MIN_DURATION        = 0.5
+MAX_ALLOWED_SILENCE = 0.2
+CLIP_DURATION       = 2
+
+ENABLE_DEBUGGING    = False
+
+
+def dprint(*args: Any) -> None:
+    if ENABLE_DEBUGGING:
+        print(*args)
+
+def skip_leading_silence(waveform: NpArray) -> NpArray:
+    """ Returns trimmed array. """
+    dprint("skip_leading_silence")
+
+    max_allowed_silence = int(MAX_ALLOWED_SILENCE * SAMPLE_RATE)
+
+    for pos, level in enumerate(waveform):
+        if abs(level) > SILENCE_THRESHOLD:
+            base = max(pos, 0)
+            dprint("skipping first %d samples" % base)
+            return waveform[base:]
+
+    # Signal is not detected, return an empty array.
+    dprint("skip_leading_silence: end of data or complete silence")
+    return np.array([])
+
+def pad(a: NpArray) -> NpArray:
+    max_clip_duration = int(CLIP_DURATION * SAMPLE_RATE)
+    pad = max_clip_duration - a.size
+
+    if pad > 0:
+        a = np.pad(a, (0, pad), mode="constant", constant_values=(0, 0))
+
+    return a
+
+def extract_next_clip(waveform: NpArray) -> Tuple[NpArray, NpArray]:
+    """ Scans the sound data until the maximum length is reached or silence
+    is detected. Returns two arrays: signal and the rest of waveform. """
+    dprint("extract_next_clip")
+
+    max_allowed_silence = int(MAX_ALLOWED_SILENCE * SAMPLE_RATE)
+    min_clip_duration   = int(MIN_DURATION * SAMPLE_RATE)
+    max_clip_duration   = int(CLIP_DURATION * SAMPLE_RATE)
+    dprint("max_clip_duration", max_clip_duration)
+    dprint("waveform length", waveform.size)
+
+    is_now_silence = False
+    silence_duration = 0
+
+    for pos, level in enumerate(waveform):
+        silence = abs(level) < SILENCE_THRESHOLD
+
+        if silence and is_now_silence:
+            silence_duration += 1
+
+            if silence_duration >= max_allowed_silence:
+                dprint("returning first %d samples because of silence" % pos)
+                return pad(waveform[:max_clip_duration]), waveform[pos:]
+        elif silence and not is_now_silence:
+            is_now_silence = True
+            silence_duration = 1
+        else:
+            is_now_silence = False
+            if pos >= max_clip_duration:
+                dprint("returning first %d samples because of duration" % pos)
+                return waveform[pos - max_clip_duration : pos], waveform[pos:]
+
+    dprint("extract_next_clip: end of data")
+    dprint("waveform.size", waveform.size, "min_clip_duration", min_clip_duration)
+
+    if waveform.size < min_clip_duration:
+        dprint("returning empty arrays")
+        return np.array([]), np.array([])
+    else:
+        return pad(waveform[:max_clip_duration]), np.array([])
+
+def break_waveform_into_clips(waveform: NpArray) -> List[NpArray]:
+    """ Breaks waveform into clips with maximum length limit while skipping
+    silence. """
+    max_clip_duration = int(CLIP_DURATION * SAMPLE_RATE)
+    clips = []
+
+    waveform /= np.max(waveform)
+
+    while waveform.size:
+        waveform = skip_leading_silence(waveform)
+        if not waveform.size:
+            break
+
+        signal, waveform = extract_next_clip(waveform)
+        dprint("extract_next_clip returned %d samples" % signal.size)
+
+        if signal.size:
+            assert(signal.size == max_clip_duration)
+            clips.append(signal)
+
+    return clips
+
+def visualize_random_file(test_file: str) -> None:
+    """ Debug visualization. """
+    waveform, sr = librosa.core.load(test_file, sr=SAMPLE_RATE)
+    assert(sr == SAMPLE_RATE)
+
+    print("analyzing file:", test_file)
+    print("waveform duration:", waveform.size)
+
+    clips = break_waveform_into_clips(waveform)
+    cols = len(clips) + 1
+
+    plt.subplot(cols + 1, 1, 1)
+    plt.plot(waveform)
+
+    for i, clip in enumerate(clips):
+        plt.subplot(cols + 1, 1, 2 + i)
+        plt.plot(clip)
+
+    plt.xlabel(test_file)
+    plt.show()
+
+    sys.exit()
+
+def read_file(path: str) -> List[NpArray]:
+    """ Reads the wav file and returns a list of np.arrays. """
+    waveform, sr = librosa.core.load(path, sr=SAMPLE_RATE)
+    assert(sr == SAMPLE_RATE)
+    data = []
+
+    if waveform.size == 0:
+        return []    # handle empty files
+
+    for clip in break_waveform_into_clips(waveform):
+        data.append(clip)
+
+    return data
+
+def load_dataset(files: List[str]) -> List[List[NpArray]]:
+    """ Loads a particular dataset. """
+    print("loading files, first 5 are:", files[:5])
+
+    # test_file = np.random.choice(files)
+    # test_file = "../data/audio_train/8f6071d2.wav"
+    # visualize_random_file(test_file)
+
+    pool = multiprocessing.Pool(processes=12)
+    data = [x for x in tqdm(pool.imap(read_file, files), total=len(files))]
+    return data
+
+
+def get_random_eraser(p: float = 0.5, s_l: float = 0.02, s_h: float = 0.4,
+                      r_1: float = 0.3, r_2: float = 1/0.3, v_l: float = 0,
+                      v_h: float = 1.0) -> Any:
+    """ An augmentation that erases part of the image. """
+    def eraser(input_img: NpArray) -> NpArray:
+        img_h, img_w, _ = input_img.shape
+        p_1 = np.random.rand()
+
+        if p_1 > p:
+            return input_img
+
+        while True:
+            s = np.random.uniform(s_l, s_h) * img_h * img_w
+            r = np.random.uniform(r_1, r_2)
+            w = int(np.sqrt(s / r))
+            h = int(np.sqrt(s * r))
+            left = np.random.randint(0, img_w)
+            top = np.random.randint(0, img_h)
+
+            if left + w <= img_w and top + h <= img_h:
+                break
+
+        c = np.random.uniform(v_l, v_h)
+        input_img[top:top + h, left:left + w, :] = c
+
+        return input_img
+
+    return eraser
+
+
+def find_files(path: str) -> List[str]:
+    """ Reads whole dataset into np.array. """
+    files = sorted(list(glob.glob(os.path.join(path, "*.wav"))))
+
+    print("files: %d files found" % len(files))
+    return files
+
+def build_train_dataset(x: List[List[NpArray]], y: NpArray, indices:
+                        List[int]) -> \
+                        Tuple[NpArray, NpArray]:
+    """ Builds dataset with multiple items per sample. Returns (x, y). """
+    res_x, res_y = [], []
+
+    for idx in indices:
+        for sample in x[idx]:
+            res_x.append(sample)
+            res_y.append(y[idx])
+
+    return np.array(res_x), np.array(res_y)
+
+def build_test_dataset(x: List[List[NpArray]]) -> Tuple[NpArray, List[int]]:
+    """ Builds test dataset with multiple clips per sample, which will
+    be joined later somehow (maximum, geom. mean, etc).
+    Returns: (x_test, clips_per_sample). """
+    x_test, clips_per_sample = [], []
+
+    for sample in x:
+        clips_per_sample.append(len(sample))
+
+        for clip in sample:
+            x_test.append(clip)
+
+    return np.array(x_test), clips_per_sample
+
+def load_data(train_idx: NpArray, val_idx: NpArray) -> \
+              Tuple[NpArray, NpArray, NpArray, NpArray, NpArray, Any, List[int]]:
+    """ Loads all data. """
+    train_df = pd.read_csv("../data/train.csv", index_col="fname")
+
+    train_cache = "../output/train_cache_v%02x.pkl" % DATA_VERSION
+    test_cache = "../output/test_cache_v%02x.pkl" % DATA_VERSION
+
+    print("reading train dataset")
+    if os.path.exists(train_cache):
+        x = pickle.load(open(train_cache, "rb"))
+    else:
+        train_files = find_files("../data/audio_train/")
+        print("len(train_files)", len(train_files))
+
+        x = load_dataset(train_files)
+        pickle.dump(x, open(train_cache, "wb"))
+
+    # get whatever data metrics we need
+    label_binarizer = LabelBinarizer()
+    label_binarizer.fit(train_df["label"])
+
+    x_train, y_train = build_train_dataset(x, train_df["label"], train_idx)
+    x_val, y_val = build_train_dataset(x, train_df["label"], val_idx)
+
+    print("reading test dataset")
+    if os.path.exists(test_cache):
+        x_test = pickle.load(open(test_cache, "rb"))
+    else:
+        test_files = find_files("../data/audio_test/")
+        print("len(test_files)", len(test_files))
+
+        x_test = load_dataset(test_files)
+        pickle.dump(x_test, open(test_cache, "wb"))
+
+    x_test, clips_per_sample = build_test_dataset(x_test)
+
+    x_joined = np.concatenate([x_train, x_val])
+    min, max = np.min(x_joined), np.max(x_joined)
+    print("data min", min, "data max", max)
+    # x_train = (x_train - min) / (max - min)
+    # x_val = (x_val - min) / (max - min)
+    # x_test = (x_test - min) / (max - min)
+
+    y_train = label_binarizer.transform(y_train)
+    y_val = label_binarizer.transform(y_val)
+
+    print("x_train.shape", x_train.shape, "y_train.shape", y_train.shape)
+    print("x_val.shape", x_val.shape, "y_val.shape", y_val.shape)
+
+    return x_train, y_train, x_val, y_val, x_test, label_binarizer, clips_per_sample
+
+def map3_metric(predict: NpArray, ground_truth: NpArray) -> float:
+    """ Implements Mean Average Precision @ Top 3. """
+    results = []
+    assert(predict.shape[0] == ground_truth.shape[0])
+
+    for actual, pred in zip(ground_truth, predict):
+        actual = np.argmax(actual)
+
+        pred = np.argsort(pred)[-TOPK:]
+        pred = np.flip(pred, axis=0)
+
+        score = 0.0
+        num_hits = 0
+
+        for i, p in enumerate(pred):
+            if p == actual and p not in pred[:i]:
+                num_hits += 1
+                score += num_hits / (i + 1)
+
+        results.append(score)
+
+    assert(len(results) == predict.shape[0])
+    return np.mean(results)
---
+++ v71_more_folds.py
@@ -0,0 +1,234 @@
+#!/usr/bin/python3.6
+
+import glob, os, pickle, sys
+from typing import *
+
+import numpy as np, pandas as pd, scipy as sp
+import keras
+
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedKFold
+from sklearn.utils import class_weight
+
+from data_v4 import find_files, load_data, map3_metric
+
+
+NpArray = Any
+
+TOPK = 3
+
+CODE_VERSION    = os.path.splitext(os.path.basename(__file__))[0][1:]
+DATA_VERSION    = 0x01
+
+PREDICT_ONLY    = False
+ENABLE_KFOLD    = True
+TEST_SIZE       = 0.2
+KFOLDS          = 20
+
+NUM_CLASSES     = 41
+
+# Network hyperparameters
+BATCH_SIZE      = 32
+NUM_EPOCHS      = 100
+
+
+def get_model_path(name: str) -> str:
+    """ Builds the path of the model file. """
+    return "../models/%s__%s.hdf5" % (CODE_VERSION, name)
+
+def get_best_model_path(name: str) -> str:
+    """ Returns the path of the best model. """
+    return get_model_path("%s_best" % name)
+
+class Map3Metric(keras.callbacks.Callback):
+    """ Keras callback that calculates MAP3 metric. """
+    def __init__(self, x_val: NpArray, y_val: NpArray, name: str) -> None:
+        self.x_val = x_val
+        self.y_val = y_val
+        self.name = name
+
+        self.best_map3 = 0.0
+        self.best_epoch = 0
+
+        self.last_best_map3 = 0.0
+        self.last_best_epoch = 0
+        self.max_epochs = 15
+        self.min_threshold = 0.001
+
+    def on_epoch_end(self, epoch: int, logs: Any = {}) -> None:
+        predict = self.model.predict(self.x_val)
+        map3 = map3_metric(predict, self.y_val)
+        print("epoch %d MAP@3 %.4f" % (epoch, map3))
+
+        if map3 > self.best_map3:
+            self.best_map3 = map3
+            self.best_epoch = epoch
+
+            self.model.save(get_model_path("%s_val_%.4f" % (self.name, map3)))
+            self.model.save(get_best_model_path(self.name))
+
+        # Optionally do early stopping, basing on MAP@3 metric
+        if self.best_map3 > self.last_best_map3 + self.min_threshold:
+            self.last_best_map3 = map3
+            self.last_best_epoch = epoch
+        elif epoch >= self.last_best_epoch + self.max_epochs:
+            self.model.stop_training = True
+            print("stopping training because MAP@3 growth has stopped")
+
+def train_model(x_train: NpArray, x_val: NpArray, y_train: NpArray, y_val:
+                NpArray, model_name: str) -> Any:
+    """ Creates model and trains it. Returns trained model. """
+    x = inp = keras.layers.Input(shape=x_train.shape[1:])
+    x = keras.layers.Reshape((x_train.shape[1], 1))(x)
+
+    x = keras.layers.Convolution1D(16, 9, activation="relu", padding="valid")(x)
+    x = keras.layers.Convolution1D(16, 9, activation="relu", padding="valid")(x)
+    x = keras.layers.MaxPool1D(16)(x)
+    x = keras.layers.Dropout(0.1)(x)
+
+    x = keras.layers.Convolution1D(32, 3, activation="relu", padding="valid")(x)
+    x = keras.layers.Convolution1D(32, 3, activation="relu", padding="valid")(x)
+    x = keras.layers.MaxPool1D(4)(x)
+    x = keras.layers.Dropout(0.1)(x)
+
+    x = keras.layers.Convolution1D(32, 3, activation="relu", padding="valid")(x)
+    x = keras.layers.Convolution1D(32, 3, activation="relu", padding="valid")(x)
+    x = keras.layers.MaxPool1D(4)(x)
+    x = keras.layers.Dropout(0.1)(x)
+
+    x = keras.layers.Convolution1D(256, 3, activation="relu", padding="valid")(x)
+    x = keras.layers.Convolution1D(256, 3, activation="relu", padding="valid")(x)
+    x = keras.layers.GlobalMaxPool1D()(x)
+    x = keras.layers.Dropout(0.2)(x)
+
+    x = keras.layers.Dense(64, activation="relu")(x)
+    x = keras.layers.Dense(1024, activation="relu")(x)
+    out = keras.layers.Dense(NUM_CLASSES, activation="softmax")(x)
+
+    model = keras.models.Model(inputs=inp, outputs=out)
+    model.summary()
+
+    adam = keras.optimizers.Adam(1e-4)
+    model.compile(loss="categorical_crossentropy", optimizer=adam,
+                  metrics=["accuracy"])
+
+    map3 = Map3Metric(x_val, y_val, model_name)
+    reduce_lr = keras.callbacks.ReduceLROnPlateau(monitor='val_loss',
+                    factor=0.33, patience=7, verbose=1, min_lr=3e-6)
+
+    model.fit(x_train, y_train, batch_size=BATCH_SIZE, epochs=NUM_EPOCHS,
+              verbose=1, validation_data=[x_val, y_val],
+              class_weight=class_weight,
+              callbacks=[map3, reduce_lr])
+
+    print("best MAP@3 value: %.04f at epoch %d" % (map3.best_map3, map3.best_epoch))
+    return model
+
+def merge_predictions(pred: NpArray, mode: str, axis: int) -> NpArray:
+    """ Merges predictions for all clips and returns a single prediction. """
+    assert(pred.shape[-1] == NUM_CLASSES)
+
+    if mode == "mean":
+        return np.mean(pred, axis=axis)
+    elif mode == "max":
+        return np.max(pred, axis=axis)
+    elif mode == "geom_mean":
+        return sp.stats.gmean(pred, axis=axis)
+    else:
+        assert(False)
+
+def predict(x_test: NpArray, label_binarizer: Any, clips_per_sample: List[int],
+            model_name: str) -> NpArray:
+    """ Predicts results on test, using the best model. """
+    print("predicting results")
+    model = keras.models.load_model(get_best_model_path(model_name))
+
+    y_test = model.predict(x_test, verbose=1)
+    print("y_test.shape after predict", y_test.shape)
+
+    pos = 0
+    y_merged = []
+
+    for count in clips_per_sample:
+        if count != 0:
+            y_merged.append(merge_predictions(y_test[pos : pos+count], "max", 0))
+        else:
+            y_merged.append(np.zeros_like(y_merged[0]))
+
+        pos += count
+
+    y_test = np.array(y_merged)
+    print("y_test.shape after merge", y_test.shape)
+    return y_test
+
+def encode_predictions(y_test: NpArray) -> NpArray:
+    """ Takes array NUM_SAMPLES x NUM_CLASSES, returns array NUM_SAMPLES
+    of strings."""
+    print("y_test.shape after merge", y_test.shape)
+
+    # extract top K values
+    y_test = np.argsort(y_test, axis=1)[:, -TOPK:]
+    y_test = np.flip(y_test, axis=1)
+    print("y_test", y_test.shape)
+
+    n_test = y_test.shape[0]
+    pred = np.zeros((n_test, TOPK), dtype=object)
+
+    for col in range(TOPK):
+        answers = keras.utils.to_categorical(y_test[:, col])
+        pred[:, col] = label_binarizer.inverse_transform(answers)
+
+    joined_pred = np.zeros(n_test, dtype=object)
+    for row in range(n_test):
+        joined_pred[row] = " ".join(pred[row, :])
+
+    return joined_pred
+
+if __name__ == "__main__":
+    train_df = pd.read_csv("../data/train.csv", index_col="fname")
+    train_indices = range(train_df.shape[0])
+
+    test_files = find_files("../data/audio_test/")
+    test_idx = [os.path.basename(f) for f in test_files]
+
+    train_labels = stratify=train_df["label"]
+    class_weight = class_weight.compute_class_weight('balanced',
+                       np.unique(train_labels), train_labels)
+    class_weight = {i: w for i, w in enumerate(class_weight)}
+    print(class_weight)
+
+    if not ENABLE_KFOLD:
+        train_idx, val_idx = train_test_split(train_indices,
+                                              stratify=train_labels,
+                                              random_state=0,
+                                              test_size=TEST_SIZE)
+        x_train, y_train, x_val, y_val, x_test, label_binarizer, \
+            clips_per_sample = load_data(train_idx, val_idx)
+
+        if not PREDICT_ONLY:
+            train_model(x_train, x_val, y_train, y_val, "nofolds")
+
+        pred = predict(x_test, label_binarizer, clips_per_sample, "nofolds")
+    else:
+        kf = StratifiedKFold(n_splits=KFOLDS, shuffle=False)
+        pred = np.zeros((len(test_idx), KFOLDS, NUM_CLASSES))
+
+        for k, (train_idx, val_idx) in enumerate(kf.split(train_indices, train_labels)):
+            print("fold %d ==============================================" % k)
+
+            x_train, y_train, x_val, y_val, x_test, label_binarizer, \
+                clips_per_sample = load_data(train_idx, val_idx)
+            name = "fold_%d" % k
+
+            if not PREDICT_ONLY:
+                train_model(x_train, x_val, y_train, y_val, name)
+
+            pred[:, k, :] = predict(x_test, label_binarizer, clips_per_sample, name)
+
+        print("before final merge: pred.shape", pred.shape)
+        pred = merge_predictions(pred, "geom_mean", axis=1)
+        print("predictions after final merge", pred.shape)
+
+    np.savez("../predictions/%s.npz" % CODE_VERSION, predict=pred)
+    print("matrix of predictions has been saved")
